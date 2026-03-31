73637140e5e0add34546b705ee6905cb6592b161
NagarajuKolar <kolarnagaraju@gmail.com>
2026-03-31 17:57:59 +0530

    Fixed cookies

--- backend/src/controllers/Usercontroller.js
+++ backend/src/controllers/Usercontroller.js
@@ -62,7 +62,8 @@ const loginUser = async (req, res) => {
 
     res.cookie("token", jwttoken, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "None",
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
--- backend/src/middleware/Auth.js
+++ backend/src/middleware/Auth.js
@@ -27,7 +27,7 @@ const authmiddleware = async (req, res, next) => {
   const token = req.cookies.token;
 
   if (!token) {
-    return res.status(401).json({ message: "Not authorized, no token" });
+    return res.status(401).json({ message: "Not authorized, Please Register" });
   }
 
   try {
